98be484cb369ea4d831959cf0f0afdc0a9785893
DennisKimani <dennisngugi.dk@gmail.com>
2017-09-19 13:04:31 +0300

    added the get methods.

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,30 @@ router.get('/', function(req, res, next) {
     res.render('index', { language: "1", langCode: "c_cpp" });
 });
 
+router.get('/java', function(req, res, next) {
+    res.render('java', { language: "1", langCode: "c_cpp" });
+});
+
+router.get('/c#', function(req, res, next) {
+    res.render('c#', { language: "1", langCode: "c_cpp" });
+});
+
+router.get('/cplus', function(req, res, next) {
+    res.render('/cplus', { language: "1", langCode: "c_cpp" });
+});
+
+router.get('/javascript', function(req, res, next) {
+    res.render('javascript', { language: "1", langCode: "c_cpp" });
+});
+
+router.get('/python', function(req, res, next) {
+    res.render('python', { language: "1", langCode: "c_cpp" });
+});
+
 router.post('/compile', function(req, res, next) {
 
     hackerRank.submit({
-        //avoid adding spaces inside your api key 
+        //avoid adding spaces inside your api key
         apiKey: 'hackerrank|2759202-1877|824f42488b7d9c61df3bb3bb333cc808a7515fdb',
         source: req.body.source,
         language: parseInt(req.body.language),
@@ -40,4 +60,11 @@ router.get('/changelang/:langCode/:language', function(req, res, next) {
 
 });
 
+router.get('/changelang/:langCode/:language', function(req, res, next) {
+    var language = req.params.language.trim();
+    var langCode = req.params.langCode.trim();
+
+    res.render('java', { language: language, langCode: langCode });
+
+});
 module.exports = router;
